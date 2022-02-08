d0efeb3c6ad3c21e36988b01c5f29f6ecb48309b
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2022-02-07 22:42:53 -0300

    UPD: melhora o model

--- src/avaportal/admin.py
+++ src/avaportal/admin.py
@@ -1,26 +1,101 @@
+from encodings import search_function
+from tabnanny import verbose
+from django.utils.translation import gettext as _
+from django.contrib.admin import register, display, ModelAdmin, TabularInline
 # from import_export.admin import ImportExportModelAdmin
-from django.contrib.admin import register, ModelAdmin, TabularInline
-from .models import Campus, Solicitacao
+from .models import Ambiente, Periodo, Polo, Componente, Diario, Campus, Curso, Turma, Solicitacao, Usuario
 
+
+#### 
+# Inlines
+####
 
 class SolicitacaoInline(TabularInline):
     model = Solicitacao
     extra = 0
 
 
+#### 
+# Admins
+####
+
+@register(Ambiente)
+class AmbienteAdmin(ModelAdmin):
+    list_display = ('nome', 'url')
+    search_fields = ('nome', 'url')
+
+
+@register(Periodo)
+class PeriodoAdmin(ModelAdmin):
+    list_display = ('ano_mes',)
+    search_fields =  ('ano_mes',)
+
+
+@register(Polo)
+class PoloAdmin(ModelAdmin):
+    list_display = ('nome', 'suap_id')
+    search_fields =  ('suap_id', 'nome')
+
+
+@register(Componente)
+class ComponenteAdmin(ModelAdmin):
+    list_display = ('suap_id', 'sigla', 'descricao', 'periodo', 'tipo', 'optativo', 'qtd_avaliacoes',)
+    list_filter = ('optativo', 'qtd_avaliacoes', 'periodo', 'tipo')
+    search_fields =  ('sigla', 'suap_id', 'descricao', 'descricao_historico', 'descricao_historico', )
+
+
 @register(Campus)
 class CampusAdmin(ModelAdmin):
-    list_display = ['sigla', 'descricao', 'active', 'homepage']
-    search_fields = ['sigla', 'descricao', 'suap_id', 'url']
-    list_filter = ['active', 'homepage']
+    list_display = ('sigla','descricao','active','homepage',)
+    list_filter = ('active', 'homepage')
+    search_fields = ('sigla', 'descricao', 'suap_id', 'url',)
     inlines = [SolicitacaoInline]
+
+
+@register(Curso)
+class CursoAdmin(ModelAdmin):
+    list_display = ('codigo', 'nome', 'suap_id', 'active', 'homepage')
+    list_filter = ('active', 'homepage')
+    search_fields = ('codigo', 'nome', 'suap_id', 'descricao',)
+
+
+@register(Turma)
+class TurmaAdmin(ModelAdmin):
+    list_display = ('codigo', 'suap_id', 'campus', 'curso', 'periodo_ano', 'periodo_mes', 'periodo_curso', 'turno', 'active')
+    list_filter = ('active', 'turno', 'periodo_ano', 'periodo_mes', 'periodo_curso', 'campus', 'curso')
+    search_fields = ('codigo', 'suap_id', 'campus', 'curso', 'periodo_ano', 'periodo_mes', 'periodo_curso', 'turno', 'active')
 
 
 @register(Solicitacao)
 class SolicitacaoAdmin(ModelAdmin):
-    list_display = ['timestamp', 'status', 'status_code', 'campus', 'resposta']
-    search_fields = ['campus', 'requisicao', 'requisicao_invalida', 'requisicao_header', 'resposta', 'resposta_header', 'resposta_invalida']
+    list_display = ('timestamp', 'status', 'status_code', 'campus', 'resposta')
+    list_filter = ('status', 'status_code', 'campus')
+    search_fields = ['campus', 'requisicao', 'requisicao_invalida', 'requisicao_header', 'resposta', 'resposta_header', 'resposta_invalida']    
     autocomplete_fields = ['campus']
     date_hierarchy = 'timestamp'
-    list_filter = ['status', 'status_code', 'campus']
     ordering = ('-timestamp',)
+
+
+@register(Diario)
+class DiarioAdmin(ModelAdmin):
+    list_display = ('codigo', 'suap_id', 'situacao', 'descricao', 'sigla', 'turma')
+    list_filter = ('situacao',)
+    search_fields =  ('codigo', 'suap_id', 'descricao', 'descricao_historico', 'sigla')
+
+
+@register(Usuario)
+class UsuarioAdmin(ModelAdmin):
+    list_display = (
+        'username', 'nome', 'email'
+    )
+    list_filter = ('polo__nome', 'campus__sigla')
+    
+    @display
+    def auth(self, obj):        
+        result = ""
+        if obj.is_staff:
+            result += 'Colaborador superusuário ' if obj.is_superuser else 'Colaborador '
+        else:
+            result += 'Usuário '
+        result += '(Ativo)' if obj.is_active else '(Inativo)'
+        return result
--- src/avaportal/models.py
+++ src/avaportal/models.py
@@ -1,70 +1,98 @@
-from django.db.models.fields import IntegerField
 from django.utils.translation import gettext as _
+from django.utils import timezone
+from django.utils.crypto import salted_hmac
 from django.conf import settings
-from django.db.models import Model, ForeignKey, CASCADE, TextChoices, BooleanField, IntegerChoices
-from django.db.models import CharField, URLField, ImageField, DateTimeField, TextField
+from django.core.mail import send_mail
+from django.contrib.auth.models import Group, UserManager, _user_get_permissions, _user_has_module_perms, _user_has_perm
+from django.db.models import Model, ForeignKey, CASCADE, ManyToManyField, BooleanField
+from django.db.models import CharField, URLField, ImageField, DateTimeField, TextField, IntegerField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_better_choices import Choices
 import qrcode
 
 
-class Turno(IntegerChoices):
-    NOTURNO = 1, _('Noturno')
-    VESPERTINO = 2, _('Vespertino')
-    MATUTINO = 3, _('Matutino')
-    EAD = 5, _('EAD')
-    DIURNO = 6, _('Diurno')
-    INTEGRAL = 7, _('Integral')
+class Turno(Choices):
+    NOTURNO = Choices.Value(_("Noturno"), value=1)
+    VESPERTINO = Choices.Value(_("Vespertino"), value=2)
+    MATUTINO = Choices.Value(_("Matutino"), value=3)
+    EAD = Choices.Value(_("EAD"), value=5)
+    DIURNO = Choices.Value(_("Diurno"), value=6)
+    INTEGRAL = Choices.Value(_("Integral"), value=7)
 
 
 class Ambiente(Model):
-    nome = CharField(_('Nome'), max_length=255)
+    nome = CharField(_('nome do ambiente'), max_length=255)
     url = URLField(_('URL'), max_length=255)
+
+    class Meta:
+        verbose_name = _("ambiente")
+        verbose_name_plural = _("ambientes")
+        ordering = ['nome']
+
+    def __str__(self):
+        return f'{self.nome}'
 
 
 class Periodo(Model):
-    ano_mes = IntegerField(_('Ano.Período'), unique=True)
+    ano_mes = IntegerField(_('ano.período'), unique=True)
+
+    class Meta:
+        verbose_name = _("período")
+        verbose_name_plural = _("períodos")
+        ordering = ['ano_mes']
+
+    def __str__(self):
+        return f'{self.ano_mes}'
 
 
 class Polo(Model):
     suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
-    nome = CharField(_('Nome'), max_length=255, unique=True)
+    nome = CharField(_('nome do pólo'), max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = _("pólo")
+        verbose_name_plural = _("pólos")
+        ordering = ['nome']
+
+    def __str__(self):
+        return f'{self.nome}'
 
 
 class Componente(Model):
     suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
-    sigla = CharField(_('Sigla'), max_length=255, unique=True)
-    token = CharField(_('Token'), max_length=255)
-    descricao = CharField(_('Descrição'), max_length=255)
-    descricao_historico = CharField(_('Descrição'), max_length=255)
-    periodo = IntegerField(_('Perído'))
-    tipo = IntegerField(_('Tipo'))
-    optativo = BooleanField(_('Optativo'))
-    qtd_avaliacoes = BooleanField(_('Qtd. avalições'))
+    sigla = CharField(_('sigla do componente'), max_length=255, unique=True)
+    token = CharField(_('token'), max_length=255)
+    descricao = CharField(_('descrição'), max_length=255)
+    descricao_historico = CharField(_('descrição no histórico'), max_length=255)
+    periodo = IntegerField(_('período'))
+    tipo = IntegerField(_('tipo'))
+    optativo = BooleanField(_('optativo'))
+    qtd_avaliacoes = IntegerField(_('qtd. avalições'))
 
+    class Meta:
+        verbose_name = _("componente")
+        verbose_name_plural = _("componentes")
+        ordering = ['sigla']
 
-class Diario(Model):
-    suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
-    situacao = CharField(_('Situação'), max_length=255)
-    descricao = CharField(_('Descrição'), max_length=255)
-    descricao_historico = CharField(_('Descrição'), max_length=255)
-    sigla = CharField(_('Sigla'), max_length=255)
+    def __str__(self):
+        return f'{self.sigla}'
 
 
 class Campus(Model):
     suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
-    sigla = CharField(_('Sigla'), max_length=255, unique=True)
-    token = CharField(_('Token'), max_length=255)
-    descricao = CharField(_('Descrição'), max_length=255)
+    sigla = CharField(_('sigla do campus'), max_length=255, unique=True)
+    token = CharField(_('token'), max_length=255)
+    descricao = CharField(_('descrição'), max_length=255)
     url = URLField(_('URL'), max_length=255)
-    thumbnail = ImageField(_('Thumbnail'), max_length=255)
-    active = BooleanField(_('Ativo?'))
-    homepage = BooleanField(_('Listar na página principal?'), default=True)
+    thumbnail = ImageField(_('thumbnail'), max_length=255)
+    active = BooleanField(_('ativo?'))
+    homepage = BooleanField(_('listar na página principal?'), default=True)
 
     class Meta:
-        verbose_name = _("Campus")
-        verbose_name_plural = _("Campi")
-        ordering = ['descricao']
+        verbose_name = _("campus")
+        verbose_name_plural = _("campi")
+        ordering = ['sigla']
 
     def __str__(self):
         return f'{self.sigla} - {self.descricao}'
@@ -78,17 +106,17 @@ class Campus(Model):
 
 class Curso(Model):
     suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
-    codigo = CharField(_('Código'), max_length=255, unique=True)
-    nome = CharField(_('Descrição'), max_length=255)
-    descricao = CharField(_('Descrição') max_length=255)
+    codigo = CharField(_('código do curso'), max_length=255, unique=True)
+    nome = CharField(_('nome do curso'), max_length=255)
+    descricao = CharField(_('descrição'), max_length=255)
     url_ppc = URLField(_('URL'), max_length=255)
-    thumbnail = ImageField(_('Thumbnail'), max_length=255)
-    active = BooleanField(_('Ativo?'))
-    homepage = BooleanField(_('Listar na página principal?'), default=True)
+    thumbnail = ImageField(_('thumbnail'), max_length=255)
+    active = BooleanField(_('ativo?'))
+    homepage = BooleanField(_('listar na página principal?'), default=True)
 
     class Meta:
-        verbose_name = _("Curso")
-        verbose_name_plural = _("Cursos")
+        verbose_name = _("curso")
+        verbose_name_plural = _("cursos")
         ordering = ['nome']
 
     def __str__(self):
@@ -97,18 +125,36 @@ class Curso(Model):
 
 class Turma(Model):
     suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
-    codigo = CharField(_('Código'), max_length=255, unique=True)
-    campus = ForeignKey(Campus)
-    curso = ForeignKey(Curso)
-    período_ano = CharField(_('Período de oferta (ano)'), max_length=255, unique=True)
-    período_mes = CharField(_('Período de oferta (mês)'), max_length=255, unique=True)
-    período_curso = CharField(_('Período do curso'), max_length=255, unique=True)
-    turno = CharField(_('Turno'), max_length=255, unique=True)
-    active = BooleanField(_('Ativo?'))
+    codigo = CharField(_('código da turma'), max_length=255, unique=True)
+    campus = ForeignKey(Campus, on_delete=CASCADE, )
+    curso = ForeignKey(Curso, on_delete=CASCADE, )
+    periodo_ano = CharField(_('período de oferta (ano)'), max_length=255)
+    periodo_mes = CharField(_('período de oferta (mês)'), max_length=255)
+    periodo_curso = CharField(_('período do curso'), max_length=255)
+    turno = IntegerField(_('turno'), choices=Turno, max_length=255)
+    active = BooleanField(_('ativo?'))
 
     class Meta:
-        verbose_name = _("Turma")
-        verbose_name_plural = _("Turmas")
+        verbose_name = _("turma")
+        verbose_name_plural = _("turmas")
+        ordering = ['codigo']
+
+    def __str__(self):
+        return f'{self.codigo}'
+
+
+class Diario(Model):
+    suap_id = CharField(_('ID no SUAP'), max_length=255, unique=True)
+    codigo = CharField(_('código do diário'), max_length=255, unique=True)
+    situacao = CharField(_('situação'), max_length=255)
+    descricao = CharField(_('descrição'), max_length=255)
+    descricao_historico = CharField(_('descrição no histórico'), max_length=255)
+    sigla = CharField(_('sigla'), max_length=255)
+    turma = ForeignKey(Turma, on_delete=CASCADE, )
+
+    class Meta:
+        verbose_name = _("diário")
+        verbose_name_plural = _("diários")
         ordering = ['codigo']
 
     def __str__(self):
@@ -116,57 +162,129 @@ class Turma(Model):
 
 
 class Solicitacao(Model):
-    class Status(TextChoices):
-        SUCESSO = 'Sucesso', _('Sucesso')
-        FALHA = 'Falha', _('Falha')
-    timestamp = DateTimeField('Quando ocorreu', auto_now_add=True)
-    requisicao = TextField('Requisição', null=True, blank=True)
-    requisicao_header = TextField('Cabeçalho da requisição', null=True, blank=True)
-    requisicao_invalida = TextField('Requisição inválida', null=True, blank=True)
-    resposta = TextField('Resposta', null=True, blank=True)
-    resposta_header = TextField('Cabeçalho da resposta', null=True, blank=True)
-    resposta_invalida = TextField('Resposta inválida', null=True, blank=True)
-    campus = ForeignKey(Campus, on_delete=CASCADE, verbose_name="Campus", null=True, blank=True)
-    status = CharField(_("Status"), max_length=255, choices=Status.choices, null=True, blank=True)
-    status_code = CharField(_("Status code"), max_length=255, null=True, blank=True)
+    class Status(Choices):
+        SUCESSO = Choices.Value(_("Noturno"),    value='S')
+        FALHA   = Choices.Value(_("Vespertino"), value='F')
+
+    timestamp = DateTimeField(_('quando ocorreu'), auto_now_add=True)
+    requisicao = TextField(_('requisição'), null=True, blank=True)
+    requisicao_header = TextField(_('cabeçalho da requisição'), null=True, blank=True)
+    requisicao_invalida = TextField(_('requisição inválida'), null=True, blank=True)
+    resposta = TextField(_('resposta'), null=True, blank=True)
+    resposta_header = TextField(_('cabeçalho da resposta'), null=True, blank=True)
+    resposta_invalida = TextField(_('resposta inválida'), null=True, blank=True)
+    campus = ForeignKey(Campus, on_delete=CASCADE, null=True, blank=True)
+    status = CharField(_("status"), max_length=255, choices=Status, null=True, blank=True)
+    status_code = CharField(_("status code"), max_length=255, null=True, blank=True)
 
     class Meta:
-        verbose_name = "Solicitação"
-        verbose_name_plural = "Solicitações"
+        verbose_name = _("Solicitação")
+        verbose_name_plural = _("Solicitações")
         ordering = ['id']
+
+    def __str__(self):
+        return f'{self.id}'
 
 
 class Usuario(Model):
-    suap_id = IntegerField(_('ID no SUAP'), unique=True)
-    nome = CharField(_('Nome'), max_length=255)
-    login = CharField(_('Login'), max_length=255)
-    email = CharField(_('E-Mail principal'), max_length=255)
-    email_institucional = CharField(_('E-Mail institucional'), max_length=255)
-    email_escolar = CharField(_('E-Mail escolar'), max_length=255)
-    email_academico = CharField(_('E-Mail academico'), max_length=255)
-    email_pessoal = CharField(_('E-Mail pessoal'), max_length=255)
-    tipo = CharField(_('Tipo'), max_length=255)
-    status = CharField(_('status'), max_length=255)
-    polo = ForeignKey(Polo, on_delete=CASCADE, verbose_name="Pólo", null=True, blank=True)
+    class Tipo(Choices):
+        INCERTO    = Choices.Value(_("Incerto"),    value='I', is_servidor=True,  is_colaborador=True,  username_length=0)
+        SERVIDOR   = Choices.Value(_("Servidor"),   value='S', is_servidor=True,  is_colaborador=True,  username_length=7)
+        TECNICO    = Choices.Value(_("Técnico"),    value='T', is_servidor=True,  is_colaborador=True,  username_length=7)
+        DOCENTE    = Choices.Value(_("Docente"),    value='D', is_servidor=True,  is_colaborador=True,  username_length=7)
+        ESTAGIARIO = Choices.Value(_("Estagiário"), value='E', is_servidor=True,  is_colaborador=True,  username_length=7)
+        PRESTADOR  = Choices.Value(_("Prestador"),  value='P', is_servidor=False, is_colaborador=True,  username_length=11)
+        ALUNO      = Choices.Value(_("Aluno"),      value='A', is_servidor=False, is_colaborador=False, username_length=15)
+
+        @classmethod
+        def get_by_length(cls, username_length):
+            for key, tipo in Usuario.Tipo.items():
+                if username_length == tipo.username_length:
+                    return tipo
+            return Usuario.Tipo.INCERTO
+        
+    username = CharField(_('username'), max_length=150, unique=True)
+    nome = CharField(_('nome do usuário'), max_length=255)
+    email = CharField(_('e-Mail principal'), max_length=255)
+    email_institucional = CharField(_('e-Mail institucional'), max_length=255)
+    email_escolar = CharField(_('e-Mail escolar'), max_length=255)
+    email_academico = CharField(_('e-Mail academico'), max_length=255)
+    email_pessoal = CharField(_('e-Mail pessoal'), max_length=255)
+    tipo = CharField(_('tipo'), max_length=255)
     campus = ForeignKey(Campus, on_delete=CASCADE, verbose_name="Campus", null=True, blank=True)
-
-
-class Inscricao(Model):
-    suap_id = IntegerField(_('ID no SUAP'), unique=True)
-    nome = CharField(_('Nome'), max_length=255)
-    login = CharField(_('Login'), max_length=255)
-    email = CharField(_('E-Mail principal'), max_length=255)
-    email_institucional = CharField(_('E-Mail institucional'), max_length=255)
-    email_escolar = CharField(_('E-Mail escolar'), max_length=255)
-    email_academico = CharField(_('E-Mail academico'), max_length=255)
-    email_pessoal = CharField(_('E-Mail pessoal'), max_length=255)
-    tipo = CharField(_('Tipo'), max_length=255)
-    status = CharField(_('status'), max_length=255)
     polo = ForeignKey(Polo, on_delete=CASCADE, verbose_name="Pólo", null=True, blank=True)
-    campus = ForeignKey(Campus, on_delete=CASCADE, verbose_name="Campus", null=True, blank=True)
 
+    groups = ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name="user_set", related_query_name="user")
+    is_staff = BooleanField(_('equipe'), default=False)
+    is_active = BooleanField(_('active'), default=True)
+    is_superuser = BooleanField(_('superusuário'), default=False)
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_socialauth_suap_user(sender, instance=None, created=False, **kwargs):
-    from social_django.models import UserSocialAuth
-    UserSocialAuth.objects.update_or_create(user=instance, defaults={'provider': 'suap', 'uid': instance.username})
+    date_joined = DateTimeField(_('date joined'), default=timezone.now)
+    last_login = DateTimeField(_('last login'), blank=True, null=True)    
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.username
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        return self.nome
+
+    def get_short_name(self):
+        return self.nome.split()[0]
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def get_user_permissions(self, obj=None):
+        return _user_get_permissions(self, obj, 'user')
+
+    def get_group_permissions(self, obj=None):
+        return _user_get_permissions(self, obj, 'group')
+
+    def get_all_permissions(self, obj=None):
+        return _user_get_permissions(self, obj, 'all')
+
+    def has_perm(self, perm, obj=None):
+        if self.is_active and self.is_superuser:
+            return True
+        return _user_has_perm(self, perm, obj)
+
+    def has_perms(self, perm_list, obj=None):
+        return all(self.has_perm(perm, obj) for perm in perm_list)
+
+    def has_module_perms(self, app_label):
+        if self.is_active and self.is_superuser:
+            return True
+        return _user_has_module_perms(self, app_label)
+
+    def natural_key(self):
+        return (self.username,)
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @classmethod
+    def get_email_field_name(cls):
+        return 'email'
+
+    @classmethod
+    def normalize_username(cls, username):
+        return username
