5a6b2234ea0fbb5b8ba04e8a4764210d79a77b52
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2022-06-10 20:58:30 -0300

    reorganiza os serviços

--- src/python/fake/manage.py
+++ src/python/fake/manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from settings import DATABASES
+import psycopg2
+import time
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
@@ -8,4 +12,19 @@ if __name__ == "__main__":
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError("ops!") from exc
+
+    if len(sys.argv) > 1 and sys.argv[1] in ['runserver', 'runserver_plus']:
+        db = DATABASES['default']
+        connection = psycopg2.connect(
+            dbname=db['NAME'],
+            user=db['USER'],
+            password=db['PASSWORD'],
+            host=db['HOST'],
+            port=db['PORT']
+        )
+        while connection.closed:
+            print(f"ERROR: Aguardando o banco {db['HOST']:db['PORT']/db['NAME']} subir")
+            time.sleep(3)
+        execute_from_command_line([sys.argv[0], 'migrate'])
+        
     execute_from_command_line(sys.argv)
