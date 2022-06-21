544d40befdab3625ce80da7a3e58ca5739f66033
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2022-06-21 20:12:20 -0300

    refactor: simplifica a leitura do manage.py

--- src/python/portal/manage.py
+++ src/python/portal/manage.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 import os
 import sys
-from settings import DATABASES
+from settings import DATABASES, DEBUG
 import psycopg2
 import time
 
+def _wait_db(db):
+    connection = psycopg2.connect(
+        dbname=db['NAME'],
+        user=db['USER'],
+        password=db['PASSWORD'],
+        host=db['HOST'],
+        port=db['PORT']
+    )
+    while connection.closed:
+        print(f"ERROR: Aguardando o banco {db['HOST']:db['PORT']/db['NAME']} subir")
+        time.sleep(3)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
@@ -14,17 +25,9 @@ if __name__ == "__main__":
         raise ImportError("ops!") from exc
 
     if len(sys.argv) > 1 and sys.argv[1] in ['runserver', 'runserver_plus']:
-        db = DATABASES['default']
-        connection = psycopg2.connect(
-            dbname=db['NAME'],
-            user=db['USER'],
-            password=db['PASSWORD'],
-            host=db['HOST'],
-            port=db['PORT']
-        )
-        while connection.closed:
-            print(f"ERROR: Aguardando o banco {db['HOST']:db['PORT']/db['NAME']} subir")
-            time.sleep(3)
+        _wait_db(DATABASES['default'])
         execute_from_command_line([sys.argv[0], 'migrate'])
+        if DEBUG:
+            execute_from_command_line([sys.argv[0], 'migrate'])
         
     execute_from_command_line(sys.argv)
