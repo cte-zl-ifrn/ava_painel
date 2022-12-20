98cb27fec0538e5b7d1f9e0a4e069736a01d256e
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2022-12-20 19:35:18 -0300

    build: add: exemplo de como usar o loaders

--- src/python/portal/settings.py
+++ src/python/portal/settings.py
@@ -105,6 +105,13 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
+            # 'loaders': [
+            #     ('django.template.loaders.cached.Loader', [
+            #         'django.template.loaders.filesystem.Loader',
+            #         'django.template.loaders.app_directories.Loader',
+            #         'path.to.custom.Loader',
+            #     ]),
+            # ],            
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
