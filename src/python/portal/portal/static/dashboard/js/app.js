2c14f28d108e6000de2266c0e901b71c2575d43c
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-02-06 18:00:12 -0300

    feat: add: mostra mensagem de erro e filtro vazio

--- src/python/portal/portal/static/dashboard/js/app.js
+++ src/python/portal/portal/static/dashboard/js/app.js
@@ -24,13 +24,13 @@ export default {
             ],
             disciplinas: [],
             cursos: [],
-            arquetipos: [],
             ambientes: [],
             coordenacoes: [],
             praticas: [],
             diarios: [],
             salas: [],
             has_error: false,
+            is_filtering: true,
         }
     },
     mounted() {
@@ -46,49 +46,43 @@ export default {
             return new Intl.DateTimeFormat('default', { dateStyle: 'long' }).format(date);
         },
         filterCards: function(a, b, c) {
-            this.filtering()
-            axios.get(
-                '/portal/api/v1/diarios/', {
-                    params: {
-                        "semestre": document.getElementById('semestre').value,
-                        "situacao": document.getElementById('situacao').value,
-                        "ordenacao": document.getElementById('ordenacao').value,
-                        "disciplina": document.getElementById('disciplina').value,
-                        "curso": document.getElementById('curso').value,
-                        "arquetipo": document.getElementById('arquetipo').value,
-                        "ambiente": document.getElementById('ambiente_id').value,
-                        "q": document.getElementById('q').value,
-                        // "page": document.getElementById('page').value,
-                        // "page_size": document.getElementById('page_size').value,
+            this.filtering();
+            try {
+                axios.get(
+                    '/portal/api/v1/diarios/', {
+                        params: {
+                            "semestre": document.getElementById('semestre').value,
+                            "situacao": document.getElementById('situacao').value,
+                            "ordenacao": document.getElementById('ordenacao').value,
+                            "disciplina": document.getElementById('disciplina').value,
+                            "curso": document.getElementById('curso').value,
+                            "ambiente": document.getElementById('ambiente_id').value,
+                            "q": document.getElementById('q').value,
+                        }
                     }
-                }
-            ).then(response => {
-                Object.assign(this, response.data);
-                console.log(response.data);
-                this.filtered();
-            }).catch(error => {
+                ).then(response => {
+                    Object.assign(this, response.data);
+                    this.filtered();
+                }).catch(error => {
+                    this.has_error = true;
+                    this.filtered();
+                    return Promise.reject(error)
+                });
+            } catch (e) {
+                console.log(e);
                 this.has_error = true;
                 this.filtered();
-                return Promise.reject(error)
-            });
-        },
-        changeCourseView: function() {
-            var newView = document.getElementById('visualizacao').value;
-            var oldView = newView=='list' ? 'cards' : 'list';
-            document.getElementById('course-' + newView).classList.remove('hide_this');
-            document.getElementById('course-' + oldView).classList.add('hide_this');
+            }
         },
         filtering: function() {
             this.diarios = [];
             this.coordenacoes = [];
             this.praticas = [];
             this.has_error = false;
-            document.getElementById('search-loader').classList.remove('hide_this');
-            document.getElementById('course-views').classList.add('hide_this');
+            this.is_filtering = true;
         },
         filtered: function() {
-            document.getElementById('search-loader').classList.add('hide_this');
-            document.getElementById('course-views').classList.remove('hide_this');
+            this.is_filtering = false;
         },
     },
 }
