4d6d47d09dafeae2f1004b189c432fb94297dd67
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-04-17 16:24:16 -0300

    feat: [fix] apenas nome_registro e username são obrigatórios

--- src/python/portal/a4/models.py
+++ src/python/portal/a4/models.py
@@ -21,20 +21,20 @@ class Usuario(AbstractUser):
                          validators=[AbstractUser.username_validator], 
                          error_messages={"unique": _("A user with that IFRN-id already exists."),})
     nome_registro = CharField(_('nome civil'), max_length=255, blank=True)
-    nome_social = CharField(_('nome social'), max_length=255, blank=True)
-    nome_usual = CharField(_('nome de apresentação'), max_length=255, blank=True)
-    nome = CharField(_('nome no SUAP'), max_length=255, blank=True)
-    tipo_usuario = CharField(_('tipo'), max_length=255, blank=True)
-    foto = CharField(_('URL da foto'), max_length=255, blank=True)
+    nome_social = CharField(_('nome social'), max_length=255, null=True, blank=True)
+    nome_usual = CharField(_('nome de apresentação'), max_length=255, null=True, blank=True)
+    nome = CharField(_('nome no SUAP'), max_length=255, null=True, blank=True)
+    tipo_usuario = CharField(_('tipo'), max_length=255, null=True, blank=True)
+    foto = CharField(_('URL da foto'), max_length=255, null=True, blank=True)
     email = EmailField(_('e-Mail preferêncial'), null=True, blank=False)
     email_secundario = EmailField(_('e-Mail pessoal'), null=True, blank=True)
-    email_corporativo = EmailField(_('e-Mail corporativo'), blank=True)
+    email_corporativo = EmailField(_('e-Mail corporativo'), null=True, blank=True)
     email_google_classroom = EmailField(_('e-Mail Gogole Classroom'), null=True, blank=True)
     email_academico = EmailField(_('e-Mail academico'), null=True, blank=True)
     campus = ForeignKey('portal.Campus', on_delete=PROTECT, verbose_name=_("campus do aluno"), null=True, blank=True)
     curso = ForeignKey('portal.Curso', on_delete=PROTECT, verbose_name=_("curso do aluno"), null=True, blank=True)
     polo = ForeignKey('portal.Polo', on_delete=PROTECT, verbose_name=_("pólo do aluno"), null=True, blank=True)
-    first_login = DateTimeField(_('first login'), blank=True, null=True)
+    first_login = DateTimeField(_('first login'), null=True, blank=True)
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
