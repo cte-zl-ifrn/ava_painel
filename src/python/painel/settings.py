1295683650ecda01d117e13e69f86761451f31d5
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-08-10 19:46:27 -0300

    release: 1.0.50

--- src/python/painel/settings.py
+++ src/python/painel/settings.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from django.core.exceptions import DisallowedHost
 
 
-PAINEL_VERSION = "1.0.49"
+PAINEL_VERSION = "1.0.50"
 
 
 BASE_DIR = Path(__file__).resolve().parent
