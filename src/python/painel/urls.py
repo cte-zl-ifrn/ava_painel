08637842381a6dfdf7f5b10c433bcf014b67933f
Kelson daCosta Medeiros <kelsoncm@gmail.com>
2023-08-11 13:02:09 -0300

    refactor: move a pasta painel para src/python

--- src/python/painel/urls.py
+++ src/python/painel/urls.py
@@ -1,43 +1,13 @@
-from django.urls import path, include, re_path
-from django.contrib import admin
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
+from django.urls import path
+from .apps import PainelConfig
+from .views import dashboard
+from painel.api import api
 
-admin.site.site_title = "Painel AVA :.: Administração"
-admin.site.site_header = admin.site.site_title
+
+app_name = PainelConfig.name
+
 
 urlpatterns = [
-    path(
-        f"{settings.ROOT_URL_PATH}",
-        include(
-            [
-                path("admin/login/", RedirectView.as_view(url="/login/")),
-                path("djrichtextfield/", include("djrichtextfield.urls")),
-                path("admin/", admin.site.urls),
-                path("", include("a4.urls")),
-                path("", include("health.urls")),
-                # path("", include("middleware.urls")),
-                path("", include("painel.urls")),
-            ]
-        ),
-    ),
-    path("", include("middleware.urls")),
-    path("", RedirectView.as_view(url=settings.ROOT_URL_PATH)),
+    path("painel/api/v1/", api.urls),
+    path("", dashboard, name="dashboard"),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += [
-        re_path("media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
-        re_path("static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
-    ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns.append(path(f"{settings.ROOT_URL_PATH}__debug__/", include(debug_toolbar.urls)))
---
+++ src/python/urls.py
@@ -0,0 +1,43 @@
+from django.urls import path, include, re_path
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
+admin.site.site_title = "Painel AVA :.: Administração"
+admin.site.site_header = admin.site.site_title
+
+urlpatterns = [
+    path(
+        f"{settings.ROOT_URL_PATH}",
+        include(
+            [
+                path("admin/login/", RedirectView.as_view(url="/login/")),
+                path("djrichtextfield/", include("djrichtextfield.urls")),
+                path("admin/", admin.site.urls),
+                path("", include("a4.urls")),
+                path("", include("health.urls")),
+                # path("", include("middleware.urls")),
+                path("", include("painel.urls")),
+            ]
+        ),
+    ),
+    path("", include("middleware.urls")),
+    path("", RedirectView.as_view(url=settings.ROOT_URL_PATH)),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path("media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        re_path("static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(path(f"{settings.ROOT_URL_PATH}__debug__/", include(debug_toolbar.urls)))
