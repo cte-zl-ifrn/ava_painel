61426e95721ad53fd2e4e9d76db9fd5d258b428f
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-09-20 15:06:13 -0300

    refactor: typping

--- src/python/painel/views.py
+++ src/python/painel/views.py
@@ -4,6 +4,6 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def dashboard(request):
+def dashboard(request: HttpRequest) -> HttpResponse:
     # https://ead.ifrn.edu.br/ava/academico/lib/ajax/service.php?info=core_course_get_enrolled_courses_by_timeline_classification
     return render(request, "painel/dashboard.html")
