6cd9076c092806a2904ae8fa42d59898acb4cf57
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-10-31 13:15:10 -0300

    release: 1.0.69

--- src/python/settings.py
+++ src/python/settings.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from django.core.exceptions import DisallowedHost
 
 
-PAINEL_VERSION = "1.0.68"
+PAINEL_VERSION = "1.0.69"
 
 
 BASE_DIR = Path(__file__).resolve().parent
