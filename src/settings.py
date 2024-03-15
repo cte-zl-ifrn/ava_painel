7b4411eb117e9332c4870b281515de0c5ede3f6b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-03-15 14:14:32 -0300

    release: avapainel:1.1.10

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from django.core.exceptions import DisallowedHost
 
 
-PAINEL_VERSION = "1.1.9"
+PAINEL_VERSION = "1.1.10"
 
 
 BASE_DIR = Path(__file__).resolve().parent
